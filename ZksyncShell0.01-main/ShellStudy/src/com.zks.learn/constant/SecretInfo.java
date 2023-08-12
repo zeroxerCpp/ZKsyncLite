e55c27853ae51049ddf3d33ba98fc0735cd2cb36
zeroxerCpp <90362622+zeroxerCpp@users.noreply.github.com>
2023-08-12 14:42:52 +0800

    Update SecretInfo.java

--- ZksyncShell0.01-main/ShellStudy/src/com.zks.learn/constant/SecretInfo.java
+++ ZksyncShell0.01-main/ShellStudy/src/com.zks.learn/constant/SecretInfo.java
@@ -1,8 +1,6 @@
 package constant;
 
 public class SecretInfo {
-    //钱包私钥
-    //0xfa48ee4b2c941e0175cc9fc82d7dfb118b8dd4101943e71b9e4c0cf287f241d6 -> 450
-    //47b78d14b06286775aa11a12d462d71b2bed017fc9f7137dee948b9cd1b1a497 ->0xe523063b918D989E4C5ef486215b0152355195a2
-    public static final String privateKey = "0xfa48ee4b2c941e0175cc9fc82d7dfb118b8dd4101943e71b9e4c0cf287f241d6";
+   
+    public static final String privateKey = "";
 }
